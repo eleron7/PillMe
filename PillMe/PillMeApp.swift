2729b0806706f85c83c321ef3ee74dd3b3f281dc
seungheon <eleron7@gmail.com>
2022-06-07 02:18:47 +0900

    Initial Commit

---
+++ PillMe/PillMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PillMeApp.swift
+//  PillMe
+//
+//  Created by 승헌 on 2022/06/07.
+//
+
+import SwiftUI
+
+@main
+struct PillMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
